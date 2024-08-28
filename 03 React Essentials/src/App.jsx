9889d037ccb83d9a181c9502b83a6283cfe7d7e1
Begana <begana.contact@gmail.com>
2024-08-28 18:56:41 +0200

    passing functions as values to props

--- 03 React Essentials/src/App.jsx
+++ 03 React Essentials/src/App.jsx
@@ -4,6 +4,10 @@ import CoreConcept from "./components/CoreConcept.jsx";
 import TapButton from "./components/TapButton.jsx";
 
 function App() {
+  function handleSelect() {
+    console.log("hello world - selected");
+  }
+
   return (
     <div>
       <Header />
@@ -24,10 +28,10 @@ function App() {
         <section id="examples">
           <h2>Examples</h2>
           <menu>
-            <TapButton>Components</TapButton>
-            <TapButton>JSX</TapButton>
-            <TapButton>Props</TapButton>
-            <TapButton>State</TapButton>
+            <TapButton onSelect={handleSelect}>Components</TapButton>
+            <TapButton onSelect={handleSelect}>JSX</TapButton>
+            <TapButton onSelect={handleSelect}>Props</TapButton>
+            <TapButton onSelect={handleSelect}>State</TapButton>
           </menu>
         </section>
       </main>
