98455c2c47ec4b4a261e3e8c92ea8b6604331115
Begana <begana.contact@gmail.com>
2024-09-08 14:13:36 +0200

    deleted

--- recap/01-starting-project-2/src/components/CoreConcept.jsx
+++
@@ -1,9 +0,0 @@
-export default function CoreConcept({ image, title, description }) {
-  return (
-    <li>
-      <h2>{title}</h2>
-      <img src={image} alt="component" />
-      <p>{description}</p>
-    </li>
-  );
-}
