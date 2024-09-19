3cf6c5b0f4421200210bbb5fbe8114a1c4e1395f
Begana <begana.contact@gmail.com>
2024-09-19 18:51:19 +0200

    setting timers

--- 08 Refs Portals/01-starting-project/src/components/TimerChallenge.jsx
+++ 08 Refs Portals/01-starting-project/src/components/TimerChallenge.jsx
@@ -1,14 +1,31 @@
+import { useState } from "react";
+
 export default function TimerChallenge({ title, targetTime }) {
+  const [timeExpired, setTimeExpired] = useState(false);
+  const [timeStarted, setTimeStarted] = useState(false);
+
+  function handleStart() {
+    setTimeStarted(true);
+
+    setTimeout(() => {
+      setTimeExpired(true);
+    }, targetTime * 1000);
+  }
   return (
     <section className="challenge">
       <h2>{title}</h2>
+      {timeExpired && "You Lose!"}
       <p className="challenge-time">
         {targetTime} second{targetTime > 1 ? "s" : ""}
       </p>
       <p>
-        <button>Start Challenge</button>
+        <button onClick={handleStart}>
+          {timeStarted ? "Stop" : "Start"} Challenge
+        </button>
       </p>
-      <p>Time is running... / Timer inactive</p>
+      <p className={timeStarted ? "active" : undefined}>
+        {timeStarted ? "Time is running..." : "Time inactive"}
+      </p>
     </section>
   );
 }
