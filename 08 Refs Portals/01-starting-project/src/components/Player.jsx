0ab2517a731357b15e9d9a3ca0368e30382ec3ff
Begana <begana.contact@gmail.com>
2024-09-19 15:14:29 +0200

    recap: two-way-binding

---
+++ 08 Refs Portals/01-starting-project/src/components/Player.jsx
@@ -0,0 +1,25 @@
+import { useState } from "react";
+
+export default function Player() {
+  const [playerName, setPlayerName] = useState("");
+  const [isSubmitted, setIsSubmitted] = useState(false);
+
+  function handleChange(event) {
+    setIsSubmitted(false);
+    setPlayerName(event.target.value);
+  }
+
+  function handleClick() {
+    setIsSubmitted(true);
+  }
+
+  return (
+    <section id="player">
+      <h2>Welcome, {isSubmitted ? playerName : "unknown entity"}!</h2>
+      <p>
+        <input type="text" onChange={handleChange} value={playerName} />
+        <button onClick={handleClick}>Set Name</button>
+      </p>
+    </section>
+  );
+}
