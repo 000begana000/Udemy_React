f2d3e14289ba2f0f1f7e3e375051b1ce56c9a735
Begana <begana.contact@gmail.com>
2024-09-25 18:40:39 +0200

    add new project to the sidebar

--- 09 Practice Project - Project Management/01-starting-project/src/App.jsx
+++ 09 Practice Project - Project Management/01-starting-project/src/App.jsx
@@ -10,6 +10,7 @@ function App() {
     projects: [],
   });
 
+  // start to write project
   function handleStartAddProject() {
     setProjectsState((prevState) => {
       return {
@@ -19,17 +20,39 @@ function App() {
     });
   }
 
+  // save the project
+  function handleAddProject(projectData) {
+    setProjectsState((prevState) => {
+      const projectId = Math.random();
+      const newProejct = {
+        ...projectData,
+        id: projectId,
+      };
+
+      return {
+        ...prevState,
+        selectedProjectId: undefined,
+        projects: [...prevState.projects, newProejct],
+      };
+    });
+  }
+
+  // console.log(projectsState);
+
   let content;
 
   if (projectsState.selectedProjectId === null) {
-    content = <NewProject />;
+    content = <NewProject onAdd={handleAddProject} />;
   } else if (projectsState.selectedProjectId === undefined) {
     content = <NoProjectSelected onStartAddProject={handleStartAddProject} />;
   }
 
   return (
     <main className="h-screen my-8 flex gap-8">
-      <ProjectsSidebar onStartAddProject={handleStartAddProject} />
+      <ProjectsSidebar
+        onStartAddProject={handleStartAddProject}
+        projects={projectsState.projects}
+      />
       {content}
     </main>
   );
