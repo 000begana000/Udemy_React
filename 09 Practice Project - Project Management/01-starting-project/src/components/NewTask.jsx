0d5ae081ee18fd8a5974e4c15b818fc37ddf6bb8
Begana <begana.contact@gmail.com>
2024-09-28 12:39:59 +0200

    debugging

--- 09 Practice Project - Project Management/01-starting-project/src/components/NewTask.jsx
+++ 09 Practice Project - Project Management/01-starting-project/src/components/NewTask.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 
 export default function NewTask({ onAdd, onDelete }) {
-  const [enteredTask, setEnteredTask] = useState();
+  const [enteredTask, setEnteredTask] = useState("");
 
   function handleChange(event) {
     setEnteredTask(event.target.value);
@@ -9,6 +9,9 @@ export default function NewTask({ onAdd, onDelete }) {
 
   // forward eneterdTask to App & empty the string
   function handleClick() {
+    if (enteredTask.trim().length === 0) {
+      return;
+    }
     onAdd(enteredTask);
     setEnteredTask("");
   }
