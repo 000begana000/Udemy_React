dba34f9be38e8d50d3ea6345311aea0c66e5415e
Begana <begana.contact@gmail.com>
2024-09-30 19:31:18 +0200

    adding automatic confirmation on deleteConfirmation by using side effect

--- 11 Side Effects useEffect/01-starting-project/src/components/DeleteConfirmation.jsx
+++ 11 Side Effects useEffect/01-starting-project/src/components/DeleteConfirmation.jsx
@@ -1,4 +1,30 @@
+import { useEffect } from "react";
+
 export default function DeleteConfirmation({ onConfirm, onCancel }) {
+  // // this code is problematic because even though we click "Cancel", it will not stop running and remove the item
+  // setTimeout(() => {
+  //   onConfirm();
+  // }, 3000);
+
+  // confirm delete automatically after 3 sec
+  useEffect(() => {
+    console.log("timer is set");
+    const timer = setTimeout(() => {
+      onConfirm();
+    }, 3000);
+
+    // can return another function which runs "right before" this event function runs again
+    // (this is not the case for this side effect function because our dependency is empty)
+    // OR RIGHT BEFORE THIS COMPONENT DISAMOUNTS (so before it's removed from the DOM)
+    return () => {
+      console.log("cleaning up timer");
+      clearTimeout(timer);
+    };
+
+    // functions are just an object value and it will re-created when the App component is re-created.
+    // but in this case, setModalIsOpen(false); will remove this component so we will not face infinite loop
+  }, [onConfirm]);
+
   return (
     <div id="delete-confirmation">
       <h2>Are you sure?</h2>
