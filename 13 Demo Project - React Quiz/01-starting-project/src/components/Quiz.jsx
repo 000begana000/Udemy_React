eea5860b60e856da38c39e86440649d41ff3a5a0
Begana <begana.contact@gmail.com>
2024-10-01 19:06:55 +0200

    add shuffle functionality to the answers

--- 13 Demo Project - React Quiz/01-starting-project/src/components/Quiz.jsx
+++ 13 Demo Project - React Quiz/01-starting-project/src/components/Quiz.jsx
@@ -3,6 +3,8 @@ import { useState } from "react";
 // always first question is correct and therefore we need to shuffle them to display
 import QUESTIONS from "../questions.js";
 
+import quizCompleteImage from "../assets/quiz-complete.png";
+
 // Show currently active question to the user
 // When user answers a question then switch to the next question
 // 1. switching between questions
@@ -13,6 +15,7 @@ export default function Quiz() {
 
   // because index starts from 0
   const activeQuestionIndex = userAnswers.length;
+  const quizIsComplete = activeQuestionIndex === QUESTIONS.length;
 
   function handleSelectedAnswer(selectedAnswer) {
     setUserAnswers((prevState) => {
@@ -20,12 +23,24 @@ export default function Quiz() {
     });
   }
 
+  if (quizIsComplete) {
+    return (
+      <div id="summary">
+        <img src={quizCompleteImage} alt="a shiny trophy image" />
+        <h2>Quiz is completed!</h2>
+      </div>
+    );
+  }
+
+  const suffledAnswers = [...QUESTIONS[activeQuestionIndex].answers];
+  suffledAnswers.sort(() => Math.random() - 0.5);
+
   return (
     <div id="quiz">
       <div id="question">
         <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
         <ul id="answers">
-          {QUESTIONS[activeQuestionIndex].answers.map((answer) => (
+          {suffledAnswers.map((answer) => (
             <li key={answer} className="answer">
               <button onClick={() => handleSelectedAnswer(answer)}>
                 {answer}
