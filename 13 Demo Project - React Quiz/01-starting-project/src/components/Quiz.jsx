2d90235b719028b3ca37d69b6ecbd3fbeaa0329c
Begana <begana.contact@gmail.com>
2024-10-01 18:30:13 +0200

    add quiz component to app component

--- 13 Demo Project - React Quiz/01-starting-project/src/components/Quiz.jsx
+++ 13 Demo Project - React Quiz/01-starting-project/src/components/Quiz.jsx
@@ -1,13 +1,39 @@
 import { useState } from "react";
 
+// always first question is correct and therefore we need to shuffle them to display
+import QUESTIONS from "../questions.js";
+
 // Show currently active question to the user
 // When user answers a question then switch to the next question
 // 1. switching between questions
 // 2. registering user answers
 
 export default function Quiz() {
-  const [activeQuestionIndex, setActiveQuestionIndex] = useState(0);
   const [userAnswers, setUserAnswers] = useState([]);
 
-  return <p>Currently Active Question</p>;
+  // because index starts from 0
+  const activeQuestionIndex = userAnswers.length;
+
+  function handleSelectedAnswer(selectedAnswer) {
+    setUserAnswers((prevState) => {
+      return [...prevState, selectedAnswer];
+    });
+  }
+
+  return (
+    <div id="quiz">
+      <div id="question">
+        <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
+        <ul id="answers">
+          {QUESTIONS[activeQuestionIndex].answers.map((answer) => (
+            <li key={answer} className="answer">
+              <button onClick={() => handleSelectedAnswer(answer)}>
+                {answer}
+              </button>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
 }
