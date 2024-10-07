cd03759855535cd89256392710e917a53a1cc33f
Begana <begana.contact@gmail.com>
2024-10-07 18:24:56 +0200

    useEffect with async await

--- 15 HTTP Requests/01-starting-project/src/components/AvailablePlaces.jsx
+++ 15 HTTP Requests/01-starting-project/src/components/AvailablePlaces.jsx
@@ -1,10 +1,27 @@
-import Places from './Places.jsx';
+import { useState, useEffect } from "react";
+
+import Places from "./Places.jsx";
 
 export default function AvailablePlaces({ onSelectPlace }) {
+  const [availablePlaces, setAvailablePlaces] = useState([]);
+
+  // TODO: fetch available places from backend API
+  useEffect(() => {
+    async function fetchPlaces() {
+      const response = await fetch("http://localhost:3000/places");
+      const resData = await response.json();
+      setAvailablePlaces(resData.places);
+    }
+
+    fetchPlaces();
+  }, []);
+
   return (
     <Places
       title="Available Places"
-      places={[]}
+      places={availablePlaces}
+      isLoading={true}
+      loadingText="Fetching place data..."
       fallbackText="No places available."
       onSelectPlace={onSelectPlace}
     />
