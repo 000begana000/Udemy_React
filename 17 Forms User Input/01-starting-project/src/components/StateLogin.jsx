fd69ee35c6599bfd1f87632a0d34417f0bff81a5
Begana <begana.contact@gmail.com>
2024-10-11 17:00:13 +0200

    validate every keystroke

--- 17 Forms User Input/01-starting-project/src/components/StateLogin.jsx
+++ 17 Forms User Input/01-starting-project/src/components/StateLogin.jsx
@@ -8,6 +8,9 @@ export default function Login() {
     password: "",
   });
 
+  const emailIsInvalid =
+    enteredValues.email !== "" && !enteredValues.email.includes("@");
+
   function handleSubmit(event) {
     event.preventDefault();
     console.log(enteredValues);
@@ -45,6 +48,9 @@ export default function Login() {
             }
             value={enteredValues.email}
           />
+          <div className="control-error">
+            {emailIsInvalid && <p>Please enter a valid email address.</p>}
+          </div>
         </div>
 
         <div className="control no-margin">
