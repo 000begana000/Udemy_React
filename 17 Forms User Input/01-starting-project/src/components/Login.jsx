4456afb9a16e701f3722f024fa69d64c1e890010
Begana <begana.contact@gmail.com>
2024-10-12 13:49:22 +0200

    form

--- 17 Forms User Input/01-starting-project/src/components/Login.jsx
+++ 17 Forms User Input/01-starting-project/src/components/Login.jsx
@@ -1,16 +1,28 @@
-import { useRef } from "react";
+import { useRef, useState } from "react";
 
 export default function Login() {
+  const [emailIsInvalid, setEmailIsInvalid] = useState(false);
+
   const email = useRef();
   const password = useRef();
 
+  // check the validation when the form is submitted
   function handleSubmit(event) {
     event.preventDefault();
 
     const enteredEmail = email.current.value;
     const enteredPassword = password.current.value;
 
-    console.log(enteredEmail, enteredPassword);
+    const emailIsValid = email.current.includes("@");
+
+    if (!emailIsValid) {
+      setEmailIsInvalid(true);
+      return; //safe clause; - secure that any http request will not be sent if the validation is invalid
+    }
+
+    setEmailIsInvalid(false);
+
+    console.log("sending http request...");
   }
 
   return (
@@ -21,6 +33,9 @@ export default function Login() {
         <div className="control no-margin">
           <label htmlFor="email">Email</label>
           <input id="email" type="email" name="email" ref={email} />
+          <div className="control-error">
+            {emailIsInvalid && <p>Please enter a valid email address</p>}
+          </div>
         </div>
 
         <div className="control no-margin">
