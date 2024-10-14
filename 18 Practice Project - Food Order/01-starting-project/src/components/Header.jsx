563ec14b5df2299bb21a8ab1d0ee863259c88dea
Begana <begana.contact@gmail.com>
2024-10-14 18:30:57 +0200

    create Header component

---
+++ 18 Practice Project - Food Order/01-starting-project/src/components/Header.jsx
@@ -0,0 +1,18 @@
+import logoImg from "../assets/logo.jpg";
+
+export default function Header() {
+  return (
+    <header id="main-header">
+      <div id="title">
+        <img
+          src={logoImg}
+          alt="graphic image glasses, napkin, and cutlets on a plate"
+        />
+        <h1>ReactFood</h1>
+      </div>
+      <nav>
+        <button>Cart (0)</button>
+      </nav>
+    </header>
+  );
+}
