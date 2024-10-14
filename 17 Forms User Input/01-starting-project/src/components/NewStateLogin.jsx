7a22014f0d3bab5619cb471e2566a1092c47cda7
Begana <begana.contact@gmail.com>
2024-10-14 16:25:33 +0200

    validation with custom component

---
+++ 17 Forms User Input/01-starting-project/src/components/NewStateLogin.jsx
@@ -0,0 +1,97 @@
+import { useState } from "react";
+import Input from "./Input.jsx";
+
+export default function Login() {
+  // const [enteredEmail, setEnteredEmail] = useState("");
+  // const [enteredPassword, setEnteredPassword] = useState("");
+  const [enteredValues, setEnteredValues] = useState({
+    email: "",
+    password: "",
+  });
+
+  const [didEdit, setDidEdit] = useState({
+    email: false,
+    password: false,
+  });
+
+  const emailIsInvalid = didEdit.email && !enteredValues.email.includes("@"); //didEdit.email => will be true after editing so we give some time to user until they type in something. even though they typed in something and it doesn't have @ sign, it's invalid
+  const passwordIsInvalid =
+    didEdit.password && enteredValues.password.trim().length < 6;
+
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    if (emailIsInvalid || passwordIsInvalid) {
+      return; //safe clause; - secure that any http request will not be sent if the validation is invalid
+    }
+
+    console.log(enteredValues);
+  }
+
+  // function handleEmailChange(event) {
+  //   setEnteredEmail(event.target.value);
+  // }
+
+  // function handlePasswordChange(event) {
+  //   setEnteredPassword(event.target.value);
+  // }
+
+  // a more generic and versatile change handling function
+  function handleChangeValues(identifier, value) {
+    setEnteredValues((prevValues) => ({
+      ...prevValues,
+      [identifier]: value,
+    }));
+
+    // when user start to type a value again, the validation goes away
+    setDidEdit((prevState) => ({
+      ...prevState,
+      [identifier]: false,
+    }));
+  }
+
+  // onBlur fires when it lost the focus
+  function handleInputBlur(identifier) {
+    setDidEdit((prevState) => ({
+      ...prevState,
+      [identifier]: true,
+    }));
+  }
+
+  return (
+    <form onSubmit={handleSubmit} noValidate>
+      <h2>Login</h2>
+
+      <div className="control-row">
+        <Input
+          label="Email"
+          id="email"
+          type="email"
+          name="email"
+          onBlur={() => handleInputBlur("email")}
+          onChange={(event) => handleChangeValues("email", event.target.value)}
+          value={enteredValues.email}
+          error={emailIsInvalid && "Please enter valid email address."}
+        />
+
+        <Input
+          label="Password"
+          id="password"
+          type="password"
+          name="password"
+          onBlur={() => handleInputBlur("password")}
+          onChange={(event) =>
+            handleChangeValues("password", event.target.value)
+          }
+          value={enteredValues.password}
+          error={passwordIsInvalid && "Please enter valid password."}
+        />
+      </div>
+
+      <p className="form-actions">
+        <button className="button button-flat">Reset</button>
+        <button className="button">Login</button>
+      </p>
+    </form>
+  );
+}
