04a7039dc1bbb5d5607fbaa2bd8ff49ce452ab2d
Begana <begana.contact@gmail.com>
2024-10-18 17:46:27 +0200

    create context and reducer, create add_item action

---
+++ 18 Practice Project - Food Order/01-starting-project/src/store/CartContext.jsx
@@ -0,0 +1,53 @@
+import { createContext, useReducer } from "react";
+
+const CartContext = createContext({
+  items: [], // manage array of items
+  addItem: (item) => {},
+  removeItem: (id) => {},
+});
+
+function cartReducer(state, action) {
+  if (action.type === "ADD_ITEM") {
+    // ... update state to add a item => state.items.push(action.item);
+    // !! we want to add items only once in cart and update quantity in the cart
+    // ... find index of already existing item
+    const existingCartItemindex = state.items.findIndex(
+      (item) => item.id === action.item.id
+    );
+
+    const updatedItems = [...state.items];
+
+    // ... if the item exists in the items array already,
+    if (existingCartItemindex > -1) {
+      const existingItem = state.items[existingCartItemindex];
+      // ... update quantity
+      const updatedItem = {
+        ...existingItem,
+        quantity: existingItem.quantity + 1,
+      };
+      updatedItems[existingCartItemindex] = updatedItem;
+    } else {
+      // ... update item
+      updatedItems.push({ ...action.item, quantity: 1 });
+    }
+
+    return { ...state, items: updatedItems }; // keep the old state and overwrite items
+  }
+
+  if (action.type === "REMOVE_ITEM") {
+    // ... update state to remove a item
+  }
+}
+
+export function CartContextProvider({ children }) {
+  const [state] = useReducer(cartReducer, { items: [] }); // items:[] initial value of the state
+
+  const ctxValue = {
+    items: [],
+  };
+  return (
+    <CartContext.Provider value={ctxValue}>{children}</CartContext.Provider>
+  );
+}
+
+export default CartContext;
