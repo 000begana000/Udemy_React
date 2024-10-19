d32a3049b93d4c590fe07ee3ef2965f43e89ac1a
Begana <begana.contact@gmail.com>
2024-10-19 16:39:20 +0200

    create header and meals component

---
+++ 18 Practice Project - Food Order/recap01/src/components/Meals.jsx
@@ -0,0 +1,30 @@
+import { useEffect, useState } from "react";
+
+export default function Meals() {
+  const [loadMeals, setLoadMeals] = useState([]);
+
+  // ... fetch data
+  useEffect(() => {
+    async function fetchMeals() {
+      const response = await fetch("http://localhost:3000/meals");
+
+      if (!response.ok) {
+        // ...
+      }
+
+      const meals = await response.json();
+      setLoadMeals(meals);
+    }
+
+    fetchMeals();
+  }, []);
+
+  // iterate with data
+  return (
+    <ul id="meals">
+      {loadMeals.map((meal) => (
+        <li key={meal.id}>{meal.name}</li>
+      ))}
+    </ul>
+  );
+}
