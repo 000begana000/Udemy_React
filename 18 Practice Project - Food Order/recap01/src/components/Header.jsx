fed35107dc3556e7422e2cf0a5856a7fd75297e6
Begana <begana.contact@gmail.com>
2024-10-20 18:25:52 +0200

    update cart number

--- 18 Practice Project - Food Order/recap01/src/components/Header.jsx
+++ 18 Practice Project - Food Order/recap01/src/components/Header.jsx
@@ -1,8 +1,16 @@
-import logoImg from "../assets/logo.jpg";
+import { useContext } from "react";
 
+import logoImg from "../assets/logo.jpg";
 import Button from "./UI/Button.jsx";
+import CartContext from "./store/CartContext.jsx";
 
 export default function Header() {
+  const cartCtx = useContext(CartContext);
+  const totalCartItems = cartCtx.items.reduce(
+    (totalNumberOfItems, item) => totalNumberOfItems + item.quantity,
+    0
+  );
+
   return (
     <header id="main-header">
       <div id="title">
@@ -10,7 +18,7 @@ export default function Header() {
         <h1>ReactFood</h1>
       </div>
       <nav>
-        <Button textOnly>Cart(0)</Button>
+        <Button textOnly>Cart({totalCartItems})</Button>
       </nav>
     </header>
   );
