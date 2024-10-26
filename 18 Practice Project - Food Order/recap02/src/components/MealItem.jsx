eec2976cc8d65d0a52880566c1c633679582d1fe
Begana <begana.contact@gmail.com>
2024-10-26 11:47:35 +0200

    apply context & reducer on the buttons

--- 18 Practice Project - Food Order/recap02/src/components/MealItem.jsx
+++ 18 Practice Project - Food Order/recap02/src/components/MealItem.jsx
@@ -1,7 +1,17 @@
+import { useContext } from "react";
+
 import { currencyFormatter } from "../util/formatting.js";
+import CartContext from "../store/CartContext.jsx";
+
 import Button from "./UI/Button.jsx";
 
 export default function MealItem({ meal }) {
+  const cartCtx = useContext(CartContext);
+
+  function handleAddMealToCart() {
+    cartCtx.addItem(meal);
+  }
+
   return (
     <li className="meal-item">
       <article>
@@ -14,7 +24,7 @@ export default function MealItem({ meal }) {
           <p className="meal-item-description">{meal.description}</p>
         </div>
         <p className="meal-item-actions">
-          <Button>Add To Cart</Button>
+          <Button onClick={handleAddMealToCart}>Add To Cart</Button>
         </p>
       </article>
     </li>
--- 18 Practice Project - Food Order/recap02/src/store/CartContext.jsx
+++ 18 Practice Project - Food Order/recap02/src/store/CartContext.jsx
@@ -13,17 +13,21 @@ function cartReducer(state, action) {
     const existingCartItemIndex = state.items.findIndex(
       (item) => item.id === action.item.id
     );
+
     const updatedItems = [...state.items];
+
     if (existingCartItemIndex > -1) {
       const existingItem = updatedItems[existingCartItemIndex];
       const updatedItem = {
         ...existingItem,
-        quantity: existingItem[quantity] + 1,
+        quantity: existingItem.quantity + 1,
       };
       updatedItems[existingCartItemIndex] = updatedItem;
     } else {
       updatedItems.push({ ...action.item, quantity: 1 });
     }
+
+    return { ...state, items: updatedItems };
   }
 
   // if quantity of item is 1, remove item
@@ -46,10 +50,12 @@ function cartReducer(state, action) {
     console.log({ ...state, items: updatedItems });
     return { ...state, itmes: updatedItems };
   }
+
+  return state;
 }
 
 export function CartContextProvider({ children }) {
-  const [cart, dispatchCartAction] = useReducer(cartReducer);
+  const [cart, dispatchCartAction] = useReducer(cartReducer, { items: [] });
 
   function addItem(item) {
     dispatchCartAction({
