ed31d939e566df79ad69217e0c36efe5d57a9bb4
Begana <begana.contact@gmail.com>
2024-10-26 12:51:32 +0200

    create Cart component

---
+++ 18 Practice Project - Food Order/recap02/src/components/Cart.jsx
@@ -0,0 +1,34 @@
+import { useContext } from "react";
+
+import CartContext from "../store/CartContext.jsx";
+
+import { currencyFormatter } from "../util/formatting.js";
+
+import Modal from "./UI/Modal.jsx";
+import Button from "./UI/Button.jsx";
+
+export default function Cart() {
+  const cartCtx = useContext(CartContext);
+  const cartTotal = cartCtx.items.reduce(
+    (totalPrice, item) => totalPrice + item.quantity * item.price,
+    0
+  );
+
+  return (
+    <Modal className="cart">
+      <h2>Your Cart</h2>
+      <ul>
+        {cartCtx.map((item) => (
+          <li key={item.id}>
+            {item.name} - {item.quantity}
+          </li>
+        ))}
+      </ul>
+      <p className="cart-total">{currencyFormatter.format(cartTotal)}</p>
+      <p className="modal-actions">
+        <Button textOnly>Close</Button>
+        <Button>Go To Checkout</Button>
+      </p>
+    </Modal>
+  );
+}
