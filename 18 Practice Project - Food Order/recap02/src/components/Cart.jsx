2258cb148eadbed6df4c4f3615ae1cf334b3f9f2
Begana <begana.contact@gmail.com>
2024-10-26 13:04:11 +0200

    implement showCart functionality

--- 18 Practice Project - Food Order/recap02/src/components/Cart.jsx
+++ 18 Practice Project - Food Order/recap02/src/components/Cart.jsx
@@ -1,6 +1,7 @@
 import { useContext } from "react";
 
 import CartContext from "../store/CartContext.jsx";
+import UserProgressContext from "../store/UserProgressContext.jsx";
 
 import { currencyFormatter } from "../util/formatting.js";
 
@@ -9,13 +10,15 @@ import Button from "./UI/Button.jsx";
 
 export default function Cart() {
   const cartCtx = useContext(CartContext);
+  const userProgressCtx = useContext(UserProgressContext);
+
   const cartTotal = cartCtx.items.reduce(
     (totalPrice, item) => totalPrice + item.quantity * item.price,
     0
   );
 
   return (
-    <Modal className="cart">
+    <Modal className="cart" open={userProgressCtx.progress === "cart"}>
       <h2>Your Cart</h2>
       <ul>
         {cartCtx.map((item) => (
