0843bbab8ad7f8bdab9f5497b26ca0c7d0c4c835
Begana <begana.contact@gmail.com>
2024-10-26 15:08:06 +0200

    implement Checkout open and close

--- 18 Practice Project - Food Order/recap02/src/components/Checkout.jsx
+++ 18 Practice Project - Food Order/recap02/src/components/Checkout.jsx
@@ -1,13 +1,32 @@
+import { useContext } from "react";
+
+import { currencyFormatter } from "../util/formatting.js";
+
+import CartContext from "../store/CartContext.jsx";
+import UserProgressContext from "../store/UserProgressContext.jsx";
+
 import Modal from "./UI/Modal.jsx";
 import Input from "./UI/Input.jsx";
 import Button from "./UI/Button.jsx";
 
 export default function Checkout() {
+  const cartCtx = useContext(CartContext);
+  const userProgressCtx = useContext(UserProgressContext);
+
+  const cartTotal = cartCtx.items.reduce(
+    (totalPrice, item) => totalPrice + item.quantity * item.price,
+    0
+  );
+
+  function handleClose() {
+    userProgressCtx.hideCheckout();
+  }
+
   return (
-    <Modal>
+    <Modal open={userProgressCtx.progress === "checkout"}>
       <form>
         <h2>Checkout</h2>
-        <p>Total Amount:</p>
+        <p>Total Amount: {currencyFormatter.format(cartTotal)}</p>
         <Input label="Full Name" type="text" id="name" />
         <Input label="E-mail Address" type="email" id="email" />
         <Input label="Street" type="text" id="street" />
@@ -16,7 +35,7 @@ export default function Checkout() {
           <Input label="City" type="text" id="city" />
         </div>
         <p className="modal-actions">
-          <Button textOnly type="button">
+          <Button textOnly type="button" onClick={handleClose}>
             Close
           </Button>
           <Button>Submit Order</Button>
