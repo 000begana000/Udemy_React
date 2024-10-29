349db8112e948427d2dc6282de7c666bb67389bb
Begana <begana.contact@gmail.com>
2024-10-29 15:04:09 +0100

    get redux data into react component

--- 20 Advanced Redux/01-starting-project/src/components/Counter.js
+++ 20 Advanced Redux/01-starting-project/src/components/Counter.js
@@ -1,12 +1,17 @@
-import classes from './Counter.module.css';
+import { useSelector } from "react-redux";
+
+import classes from "./Counter.module.css";
 
 const Counter = () => {
+  // this function will be excuted by react redux, to retrieve the part of state we need in this component.
+  const counter = useSelector((state) => state.counter);
+
   const toggleCounterHandler = () => {};
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>-- COUNTER VALUE --</div>
+      <div className={classes.value}>{counter}</div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
   );
