dfb6480fcec9c87e5539b64ccc9e86a3c9316951
Begana <begana.contact@gmail.com>
2024-10-29 16:31:43 +0100

    how to use redux in class-based component

--- 20 Advanced Redux/01-starting-project/src/components/Counter.js
+++ 20 Advanced Redux/01-starting-project/src/components/Counter.js
@@ -1,3 +1,6 @@
+// import { Component } from "react";
+// import { conect } from "react-redux";
+
 import { useSelector, useDispatch } from "react-redux";
 
 import classes from "./Counter.module.css";
@@ -31,3 +34,47 @@ const Counter = () => {
 };
 
 export default Counter;
+
+// class Counter extends Component {
+//   incrementHandler() {
+//     this.props.increment();
+//   }
+
+//   decrementHandler() {
+//     this.props.decrement();
+//   }
+
+//   toggleCounterHandler() {}
+//   render() {
+//     return (
+//       <main className={classes.counter}>
+//         <h1>Redux Counter</h1>
+//         <div className={classes.value}>{this.props.counter}</div>
+//         <div>
+//           <button onClick={this.incrementHandler.bind(this)}>Increment</button>
+//           <button onClick={this.decrementHandler.bind(this)}>Decrement</button>
+//         </div>
+//         <button onClick={this.toggleCounterHandler}>Toggle Counter</button>
+//       </main>
+//     );
+//   }
+// }
+
+// // a function that maps Redux state to props which will be received in this component then.
+// // we must use this name for it
+// const mapStateToProps = (state) => {
+//   // it returns a object
+//   return {
+//     counter: state.counter, // we pick up conter state and bind it to counter prop (equivalent to useSelector)
+//   };
+// };
+
+// // equivalent to useDispatch
+// const mapDispatchToProps = (dispatch) => {
+//   return {
+//     increment: () => dispatch({ type: "increment" }),
+//     decrement: () => dispatch({ type: "decrement" }),
+//   };
+// };
+
+// export default connect(mapStateToProps, mapDispatchToProps)(Counter);
