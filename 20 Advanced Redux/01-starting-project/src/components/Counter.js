7388ed516394f910c76a2fbea4e9726ab4ca4ebd
Begana Choi <begana.contact@gmail.com>
2024-11-02 11:56:10 +0100

    dispatch actions

--- 20 Advanced Redux/01-starting-project/src/components/Counter.js
+++ 20 Advanced Redux/01-starting-project/src/components/Counter.js
@@ -2,6 +2,7 @@
 // import { conect } from "react-redux";
 
 import { useSelector, useDispatch } from "react-redux";
+import { counterActions } from "../store/index";
 
 import classes from "./Counter.module.css";
 
@@ -12,19 +13,19 @@ const Counter = () => {
   const show = useSelector((state) => state.showCounter);
 
   const incrementHandler = () => {
-    dispatch({ type: "increment" });
+    dispatch(counterActions.increment());
   };
 
   const increaseHandler = () => {
-    dispatch({ type: "increase", amount: 10 }); //amount = name of payload
+    dispatch(counterActions.increase(10)); //{type: SOME_UNIQUE_IDENTIFIER, payload: 10}
   };
 
   const decrementHandler = () => {
-    dispatch({ type: "decrement" });
+    dispatch(counterActions.decrement());
   };
 
   const toggleCounterHandler = () => {
-    dispatch({ type: "toggle" });
+    dispatch(counterActions.toggleCounter());
   };
 
   return (
--- 20 Advanced Redux/01-starting-project/src/store/index.js
+++ 20 Advanced Redux/01-starting-project/src/store/index.js
@@ -1,13 +1,9 @@
-// import { Component } from "react";
-// import { conect } from "react-redux";
-
-import { createStore } from "redux";
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, configureStore } from "@reduxjs/toolkit";
 
 const initialState = { counter: 0, showCounter: true };
 
 // preparing slice of global state
-createSlice({
+const counterSlice = createSlice({
   name: "counter",
   initialState, //initialState: initialState
   // methods - we don't need actions becuase these methods will automatically be called for you depending on which action was triggered.
@@ -19,7 +15,7 @@ createSlice({
       state.counter--;
     },
     increase(state, action) {
-      state.counter = state.counter + action.amount;
+      state.counter = state.counter + action.payload;
     },
     toggleCounter(state) {
       state.showCounter = !state.showCounter;
@@ -27,82 +23,10 @@ createSlice({
   },
 });
 
-const counterReducer = (state = initialState, action) => {
-  if (action.type === "increment") {
-    return {
-      counter: state.counter + 1,
-      showCounter: state.showCounter,
-    };
-  }
+const store = configureStore({
+  reducer: counterSlice.reducer,
+});
 
-  if (action.type === "increase") {
-    return {
-      counter: state.counter + action.amount,
-      showCounter: state.showCounter,
-    };
-  }
-
-  if (action.type === "decrement") {
-    return {
-      counter: state.counter - 1,
-      showCounter: state.showCounter,
-    };
-  }
-
-  if (action.type === "toggle") {
-    return {
-      showCounter: !state.showCounter,
-      counter: state.counter,
-    };
-  }
-
-  return state;
-};
-
-const store = createStore(counterReducer);
+export const counterActions = counterSlice.actions;
 
 export default store;
-
-// class Counter extends Component {
-//   incrementHandler() {
-//     this.props.increment();
-//   }
-
-//   decrementHandler() {
-//     this.props.decrement();
-//   }
-
-//   toggleCounterHandler() {}
-//   render() {
-//     return (
-//       <main className={classes.counter}>
-//         <h1>Redux Counter</h1>
-//         <div className={classes.value}>{this.props.counter}</div>
-//         <div>
-//           <button onClick={this.incrementHandler.bind(this)}>Increment</button>
-//           <button onClick={this.decrementHandler.bind(this)}>Decrement</button>
-//         </div>
-//         <button onClick={this.toggleCounterHandler}>Toggle Counter</button>
-//       </main>
-//     );
-//   }
-// }
-
-// // a function that maps Redux state to props which will be received in this component then.
-// // we must use this name for it
-// const mapStateToProps = (state) => {
-//   // it returns a object
-//   return {
-//     counter: state.counter, // we pick up conter state and bind it to counter prop (equivalent to useSelector)
-//   };
-// };
-
-// // equivalent to useDispatch
-// const mapDispatchToProps = (dispatch) => {
-//   return {
-//     increment: () => dispatch({ type: "increment" }),
-//     decrement: () => dispatch({ type: "decrement" }),
-//   };
-// };
-
-// export default connect(mapStateToProps, mapDispatchToProps)(Counter);
