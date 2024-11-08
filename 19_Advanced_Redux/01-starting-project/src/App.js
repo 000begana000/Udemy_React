c0d3e0e1641bc6a4da667cbdad6241aead75f722
Begana Choi <begana.contact@gmail.com>
2024-11-08 18:23:28 +0100

    sending http request in App

--- 19_Advanced_Redux/01-starting-project/src/App.js
+++ 19_Advanced_Redux/01-starting-project/src/App.js
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import { useSelector } from "react-redux";
 
 import Cart from "./components/Cart/Cart";
@@ -6,6 +7,16 @@ import Products from "./components/Shop/Products";
 
 function App() {
   const showCart = useSelector((state) => state.ui.cartIsVisible);
+  const cart = useSelector((state) => state.cart);
+
+  useEffect(() => {
+    fetch("https://dummy-59bc5-default-rtdb.firebaseio.com/cart.json", {
+      // PUT method will overwrite cart data with new cart data
+      method: "PUT",
+      body: JSON.stringify(cart),
+    });
+  }, [cart]);
+
   return (
     <Layout>
       {showCart && <Cart />}
--- 19_Advanced_Redux/01-starting-project/src/components/Shop/ProductItem.js
+++ 19_Advanced_Redux/01-starting-project/src/components/Shop/ProductItem.js
@@ -1,58 +1,27 @@
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
 
-import { cartActions } from "../../store/cart-slice";
+import { cartActions } from "../../store/cart-slice.js";
+
 import Card from "../UI/Card";
 import classes from "./ProductItem.module.css";
 
 const ProductItem = (props) => {
   // select cart state
-  const cart = useSelector((state) => state.cart);
+
   const dispatch = useDispatch();
 
   const { title, price, description, id } = props;
 
   const addToCartHandler = () => {
-    // not to mutate the data we create new variable
-    const newTotalQuantity = cart.totalQuantity + 1;
-
-    const updatedItems = cart.items.slice(); // create copy via slice to avoid mutating original state
-    const existingItem = updatedItems.find((item) => item.id === id); // not a new object until copying in memory
-    if (existingItem) {
-      const updatedItem = { ...existingItem }; // new object + copy existing properties to avoid state mutation => brand new object in memory
-      updatedItem.quantity++;
-      updatedItem.totalPrice = updatedItem.totalPrice + price;
-      const existingItemIndex = updatedItems.findIndex(
-        (item) => item.id === id
-      );
-      updatedItems[existingItemIndex] = updatedItem;
-    } else {
-      // if the item doesn't exist
-      updatedItems.push({
-        id: id,
-        price: price,
-        quantity: 1,
-        totalPrice: price,
-        name: title,
-      });
-    }
-
-    const newCart = {
-      totalQuantity: newTotalQuantity,
-      items: updatedItems,
-    };
-
-    dispatch(cartActions.replaceCart(newCart));
-
     // and then send Http request
     // fetch('firebase-url', { method: 'POST', body: JSON.stringify(newCart) })
-
-    // dispatch(
-    //   cartActions.addItemToCart({
-    //     id,
-    //     title,
-    //     price,
-    //   })
-    // );
+    dispatch(
+      cartActions.addItemToCart({
+        id,
+        title,
+        price,
+      })
+    );
   };
 
   return (
