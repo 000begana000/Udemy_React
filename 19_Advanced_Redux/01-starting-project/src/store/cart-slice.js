7bfd6a4824b4a3e6117802b18712e7ef055153f2
Begana <begana.contact@gmail.com>
2024-11-12 15:54:23 +0100

    action creator thunk

--- 19_Advanced_Redux/01-starting-project/src/store/cart-slice.js
+++ 19_Advanced_Redux/01-starting-project/src/store/cart-slice.js
@@ -1,5 +1,7 @@
 import { createSlice } from "@reduxjs/toolkit";
 
+import { uiActions } from "./ui-slice";
+
 const initialState = { items: [], totalQuantity: 0 };
 
 const cartSlice = createSlice({
@@ -40,6 +42,53 @@ const cartSlice = createSlice({
     },
   },
 });
+
+export const sendCartData = (cart) => {
+  return async (dispatch) => {
+    dispatch(
+      uiActions.showNotification({
+        status: "pending",
+        title: "Sending...",
+        message: "Sending cart data!",
+      })
+    );
+
+    const senddRequest = async () => {
+      const response = await fetch(
+        "https://dummy-59bc5-default-rtdb.firebaseio.com/cart.json",
+        {
+          // PUT method will overwrite cart data with new cart data
+          method: "PUT",
+          body: JSON.stringify(cart),
+        }
+      );
+
+      if (!response.ok) {
+        throw new Error("Sending cart data failed");
+      }
+    };
+
+    try {
+      await senddRequest();
+
+      dispatch(
+        uiActions.showNotification({
+          status: "success",
+          title: "Success!",
+          message: "Sent cart data successfully!",
+        })
+      );
+    } catch (error) {
+      dispatch(
+        uiActions.showNotification({
+          status: "error",
+          title: "Error!",
+          message: "Sending cart data failed!",
+        })
+      );
+    }
+  };
+};
 
 export default cartSlice;
 export const cartActions = cartSlice.actions;
