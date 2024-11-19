7e7e26f83d2187ee2c1ceb3aeb992c2cd0bac0de
Begana <begana.contact@gmail.com>
2024-11-19 16:46:54 +0100

    update page without sending new request

--- 21_Routing/01-starting-project/src/App.js
+++ 21_Routing/01-starting-project/src/App.js
@@ -1,29 +1,29 @@
 import {
   createBrowserRouter,
   RouterProvider,
-  createRoutesFromElements,
-  Route,
+  // createRoutesFromElements,
+  // Route,
 } from "react-router-dom";
 
 import HomePage from "./pages/Home";
 import ProductsPage from "./pages/Products";
 
-const routeDefinition = createRoutesFromElements(
-  <Route>
-    <Route path="/" element={<HomePage />} />
-    <Route path="/products" element={<ProductsPage />} />
-  </Route>
-);
+// const routeDefinition = createRoutesFromElements(
+//   <Route>
+//     <Route path="/" element={<HomePage />} />
+//     <Route path="/products" element={<ProductsPage />} />
+//   </Route>
+// );
 
-// const router = createBrowserRouter([
-//   {
-//     path: "/",
-//     element: <HomePage />,
-//   },
-//   { path: "/products", element: <ProductsPage /> },
-// ]);
+// const router = createBrowserRouter(routeDefinition);
 
-const router = createBrowserRouter(routeDefinition);
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomePage />,
+  },
+  { path: "/products", element: <ProductsPage /> },
+]);
 
 function App() {
   return <RouterProvider router={router} />; // the value of router should be const router above
--- 21_Routing/01-starting-project/src/pages/Home.js
+++ 21_Routing/01-starting-project/src/pages/Home.js
@@ -1,5 +1,14 @@
+import { Link } from "react-router-dom";
+
 function HomePage() {
-  return <h1>My Home Page</h1>;
+  return (
+    <>
+      <h1>My Home Page</h1>
+      <p>
+        go to <Link to="/products">Product page</Link>
+      </p>
+    </>
+  );
 }
 
 export default HomePage;
