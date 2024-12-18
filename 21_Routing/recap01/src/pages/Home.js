53ed07f9dd36e605ed23e5fc651bef7e0d93452c
Begana <begana.contact@gmail.com>
2024-12-18 19:13:18 +0100

    link

--- 21_Routing/recap01/src/pages/Home.js
+++ 21_Routing/recap01/src/pages/Home.js
@@ -1,3 +1,10 @@
+import { Link } from "react-router-dom";
+
 export default function HomePage() {
-  return <h1>My Homepage</h1>;
+  return (
+    <>
+      <h1>My Homepage</h1>
+      <Link to="/products">the list of products</Link>
+    </>
+  );
 }
