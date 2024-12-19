4ffb7d9da5db4a4ad9c49d50b7e508eb2288cba4
Begana <begana.contact@gmail.com>
2024-12-19 17:19:37 +0100

    useNavigate

--- 21_Routing/recap01/src/pages/Home.js
+++ 21_Routing/recap01/src/pages/Home.js
@@ -1,10 +1,18 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 export default function HomePage() {
+  const navigate = useNavigate();
+
+  function navigateHandler() {
+    navigate("/products");
+  }
   return (
     <>
       <h1>My Homepage</h1>
       <Link to="/products">the list of products</Link>
+      <p>
+        <button onClick={navigateHandler}>Navigate</button>
+      </p>
     </>
   );
 }
