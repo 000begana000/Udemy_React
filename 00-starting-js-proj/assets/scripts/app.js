294ca529bd1ee998e5fb0db4f69c70a30f030666
Begana <begana.contact@gmail.com>
2025-01-31 15:54:49 +0100

    let and const

--- 00-starting-js-proj/assets/scripts/app.js
+++ 00-starting-js-proj/assets/scripts/app.js
@@ -2,7 +2,12 @@
 // import apiKey from "./util.js";
 // import * as util from 'util.js"
 
-import { apiKey, abc as content } from "./util.js";
+//import { apiKey, abc as content } from "./util.js";
 
 // console.log(util.apiKey)
-console.log(apiKey, content);
+// console.log(apiKey, content);
+
+const userMessage = "hello world!";
+
+console.log(userMessage);
+console.log(userMessage);
