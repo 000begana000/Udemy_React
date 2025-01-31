bcee835799221fac0119027175ce1fbe5dfa4f40
Begana <begana.contact@gmail.com>
2025-01-31 16:04:26 +0100

    function

--- 00-starting-js-proj/assets/scripts/app.js
+++ 00-starting-js-proj/assets/scripts/app.js
@@ -7,7 +7,19 @@
 // console.log(util.apiKey)
 // console.log(apiKey, content);
 
-const userMessage = "hello world!";
+// const userMessage = "hello world!";
 
-console.log(userMessage);
-console.log(userMessage);
+// console.log(userMessage);
+// console.log(userMessage);
+
+function createGreeting(userName, message = "hello!") {
+  //   console.log(userName);
+  //   console.log(message);
+  return "Hi, I am " + userName + ". " + message;
+}
+
+const greeting1 = createGreeting("Begana");
+console.log(greeting1);
+
+const greeting2 = createGreeting("max", "what's up");
+console.log(greeting2);
