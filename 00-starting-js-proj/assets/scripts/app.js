2c9223d1755c4fb5d4a3db4c841f9ec04dc5e54a
Begana <begana.contact@gmail.com>
2025-01-31 16:09:28 +0100

    arrow function

--- 00-starting-js-proj/assets/scripts/app.js
+++ 00-starting-js-proj/assets/scripts/app.js
@@ -12,14 +12,19 @@
 // console.log(userMessage);
 // console.log(userMessage);
 
-function createGreeting(userName, message = "hello!") {
-  //   console.log(userName);
-  //   console.log(message);
-  return "Hi, I am " + userName + ". " + message;
-}
+// function createGreeting(userName, message = "hello!") {
+//   //   console.log(userName);
+//   //   console.log(message);
+//   return "Hi, I am " + userName + ". " + message;
+// }
 
-const greeting1 = createGreeting("Begana");
-console.log(greeting1);
+// const greeting1 = createGreeting("Begana");
+// console.log(greeting1);
 
-const greeting2 = createGreeting("max", "what's up");
-console.log(greeting2);
+// const greeting2 = createGreeting("max", "what's up");
+// console.log(greeting2);
+
+export default (userName, message) => {
+  console.log("hello");
+  return userName + message;
+};
