b5b0869005850871bbf117c7eda4fff5888402c1
Begana <begana.contact@gmail.com>
2025-02-04 14:20:54 +0100

    end of js recap

--- 00-starting-js-proj/assets/scripts/app.js
+++ 00-starting-js-proj/assets/scripts/app.js
@@ -102,22 +102,32 @@
 //   console.log(hobby);
 // }
 
-function handleTimeout() {
-  console.log("Timed out!");
+// function handleTimeout() {
+//   console.log("Timed out!");
+// }
+
+// const handleTimeout2 = () => {
+//   console.log("Timed out.. again!");
+// };
+
+// setTimeout(handleTimeout2, 2000);
+// setTimeout(handleTimeout, 3000);
+// setTimeout(() => {
+//   console.log("More timing out....");
+// }, 4000);
+
+// function greeter(greetFn) {
+//   greetFn();
+// }
+
+// greeter(() => console.log("Hi"));
+
+function init() {
+  function greet() {
+    console.log("Hi");
+  }
+
+  greet();
 }
 
-const handleTimeout2 = () => {
-  console.log("Timed out.. again!");
-};
-
-setTimeout(handleTimeout2, 2000);
-setTimeout(handleTimeout, 3000);
-setTimeout(() => {
-  console.log("More timing out....");
-}, 4000);
-
-function greeter(greetFn) {
-  greetFn();
-}
-
-greeter(() => console.log("Hi"));
+init();
