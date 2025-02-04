996144ff794dad825f55155486afa44b7700e65c
Begana <begana.contact@gmail.com>
2025-02-04 14:05:06 +0100

    using functions as value

--- 00-starting-js-proj/assets/scripts/app.js
+++ 00-starting-js-proj/assets/scripts/app.js
@@ -96,8 +96,28 @@
 
 // console.log(extendedUser);
 
-const hobbies = ["Sports", "Cooking"];
+// const hobbies = ["Sports", "Cooking"];
 
-for (const hobby of hobbies) {
-  console.log(hobby);
+// for (const hobby of hobbies) {
+//   console.log(hobby);
+// }
+
+function handleTimeout() {
+  console.log("Timed out!");
 }
+
+const handleTimeout2 = () => {
+  console.log("Timed out.. again!");
+};
+
+setTimeout(handleTimeout2, 2000);
+setTimeout(handleTimeout, 3000);
+setTimeout(() => {
+  console.log("More timing out....");
+}, 4000);
+
+function greeter(greetFn) {
+  greetFn();
+}
+
+greeter(() => console.log("Hi"));
