b1341bf88310b849dae3b0da96dcaef078f46bb6
Begana <begana.contact@gmail.com>
2025-02-05 14:51:37 +0100

    passing custom arguments

--- 000-recap/03/src/App.jsx
+++ 000-recap/03/src/App.jsx
@@ -5,8 +5,12 @@ import CoreConcept from "./components/CoreConcept.jsx";
 import TabButton from "./components/TabButton.jsx";
 
 function App() {
-  function handleSelect() {
-    console.log("from handle Select");
+  let tabContent = "Please click a button";
+
+  function handleSelect(selectedButton) {
+    // selectedButton => 'components', 'jsx' ...
+    tabContent = selectedButton;
+    console.log(tabContent);
   }
 
   return (
@@ -29,11 +33,14 @@ function App() {
         <section id="examples">
           <h2>Examples</h2>
           <menu>
-            <TabButton onSelect={handleSelect}>Components</TabButton>
-            <TabButton onSelect={handleSelect}>JSX</TabButton>
-            <TabButton onSelect={handleSelect}>Props</TabButton>
-            <TabButton onSelect={handleSelect}>State</TabButton>
+            <TabButton onSelect={() => handleSelect("components")}>
+              Components
+            </TabButton>
+            <TabButton onSelect={() => handleSelect("jsx")}>JSX</TabButton>
+            <TabButton onSelect={() => handleSelect("props")}>Props</TabButton>
+            <TabButton onSelect={() => handleSelect("state")}>State</TabButton>
           </menu>
+          {tabContent}
         </section>
       </main>
     </div>
