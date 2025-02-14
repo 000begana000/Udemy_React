81c3a112c300f24431b4acdbd50241b07a494456
Begana <begana.contact@gmail.com>
2025-02-14 13:36:05 +0100

    create Gameboard component

--- 000-recap/04-02/src/components/GameBoard.jsx
+++ 000-recap/04-02/src/components/GameBoard.jsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 const initialGameBoard = [
   [null, null, null],
   [null, null, null],
@@ -5,6 +7,14 @@ const initialGameBoard = [
 ];
 
 export default function GameBoard() {
+  const [gameBoard, setGameBoard] = useState(initialGameBoard);
+
+  function handleSelectSquare(rowIndex, colIndex) {
+    setGameBoard((prevGameBoard) => {
+      prevGameBoard[rowIndex][colIndex] = "X";
+      return prevGameBoard;
+    });
+  }
   return (
     <ol id="game-board">
       {initialGameBoard.map((row, rowIndex) => (
@@ -12,7 +22,7 @@ export default function GameBoard() {
           <ol>
             {row.map((playerSymbol, colIndex) => (
               <li key={colIndex}>
-                <button>{playerSymbol}</button>
+                <button onClick={handleSelectSquare}>{playerSymbol}</button>
               </li>
             ))}
           </ol>
