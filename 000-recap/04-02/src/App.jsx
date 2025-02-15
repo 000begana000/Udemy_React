e3606a8c8f7991e4727f1c6d6f3fddbb5844a207
Begana <begana.contact@gmail.com>
2025-02-15 11:30:02 +0100

    draw situation

--- 000-recap/04-02/src/App.jsx
+++ 000-recap/04-02/src/App.jsx
@@ -3,6 +3,7 @@ import { useState } from "react";
 import Player from "./components/Player.jsx";
 import GameBoard from "./components/GameBoard.jsx";
 import Log from "./components/Log.jsx";
+import GameOver from "./components/GameOver.jsx";
 import { WINNING_COMBINATIONS } from "./winning-combinations.js";
 
 const initialGameBoard = [
@@ -59,6 +60,8 @@ function App() {
     }
   }
 
+  const hasDraw = gameTurns.length === 9 && !winner;
+
   function handleSelectSquare(rowIndex, colIndex) {
     // setActivePlayer((curActivePlayer) => (curActivePlayer === "X" ? "O" : "X"));
     setGameTurns((prevTurns) => {
@@ -90,7 +93,7 @@ function App() {
             isActive={activePlayer === "O"}
           />
         </ol>
-        {winner && <p>You won, {winner}!</p>}
+        {(winner || hasDraw) && <GameOver winner={winner} />}
         {/* squar get selected here */}
         <GameBoard onSelectSquare={handleSelectSquare} board={gameBoard} />
       </div>
