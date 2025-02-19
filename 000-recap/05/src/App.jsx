03037e7f27b4381b4ab7897a15fee87e830cece1
Begana <begana.contact@gmail.com>
2025-02-19 13:05:21 +0100

    outputting content conditionally

--- 000-recap/05/src/App.jsx
+++ 000-recap/05/src/App.jsx
@@ -20,6 +20,8 @@ function App() {
     duration: 10,
   });
 
+  const inputIsValid = userInput.duration >= 1;
+
   function handleChange(inputIdentifier, newValue) {
     setUserInput((prevUserInput) => {
       return {
@@ -32,7 +34,10 @@ function App() {
   return (
     <>
       <UserInput onChange={handleChange} userInput={userInput} />
-      <Results userInput={userInput} />
+      {!inputIsValid && (
+        <p className="center">Please enter a duration greater than zero.</p>
+      )}
+      {inputIsValid && <Results userInput={userInput} />}
     </>
   );
 }
