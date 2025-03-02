d7346e5b4e21f04fb6c7e10d5203784227bc437f
000begana000 <begana.contact@gmail.com>
2025-03-02 16:42:27 +0100

    two way binding recap

--- 000-recap/08/src/components/Player.jsx
+++ 000-recap/08/src/components/Player.jsx
@@ -1,10 +1,23 @@
+import { useState } from 'react';
 export default function Player() {
+  const [userName, SetUserName] = useState('');
+  const [submitted, setSubmitted] = useState(false);
+
+  function handleChange(event) {
+    setSubmitted(false);
+    SetUserName(event.target.value);
+  }
+
+  function handleClick() {
+    setSubmitted(true);
+  }
+
   return (
-    <section id="player">
-      <h2>Welcome unknown entity</h2>
+    <section id='player'>
+      <h2>Welcome {submitted ? userName : 'unknown entity'}</h2>
       <p>
-        <input type="text" />
-        <button>Set Name</button>
+        <input type='text' onChange={handleChange} value={userName} />
+        <button onClick={handleClick}>Set Name</button>
       </p>
     </section>
   );
