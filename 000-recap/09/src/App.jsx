874e161efc551719a4a15cf5316cbfde5da61987
000begana000 <begana.contact@gmail.com>
2025-03-08 10:57:22 +0100

    new project

--- 000-recap/09/src/App.jsx
+++ 000-recap/09/src/App.jsx
@@ -1,9 +1,23 @@
+import { useState } from "react";
+
 import ProjectsSidebar from "./components/ProjectsSidebar.jsx";
+import NewProject from "./components/NewProject.jsx";
+
+// button clicks = New Project
+// default = fallback area
+// fetch values and create new project
 
 function App() {
+  const [isAdding, setIsAdding] = useState(false);
+
+  function handleAddNewProject() {
+    console.log("clicked");
+  }
+
   return (
     <main className="h-screen my-8 flex gap-8">
       <ProjectsSidebar />
+      <NewProject />
     </main>
   );
 }
