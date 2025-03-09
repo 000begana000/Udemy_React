3c7499b05a84970dd1e1d332a43f69c77502787e
000begana000 <begana.contact@gmail.com>
2025-03-09 14:54:36 +0100

    fallback page

--- 000-recap/09/src/App.jsx
+++ 000-recap/09/src/App.jsx
@@ -2,22 +2,25 @@ import { useState } from "react";
 
 import ProjectsSidebar from "./components/ProjectsSidebar.jsx";
 import NewProject from "./components/NewProject.jsx";
+import NoProject from "./components/NoProjectSelected.jsx";
+import NoProjectSelected from "./components/NoProjectSelected.jsx";
 
 // button clicks = New Project
-// default = fallback area
+// default = fallback area (done)
 // fetch values and create new project
 
 function App() {
-  const [isAdding, setIsAdding] = useState(false);
+  const [isCreatingNewProject, setIsCreatingNewProject] = useState(false);
 
   function handleAddNewProject() {
-    console.log("clicked");
+    setIsCreatingNewProject(prevState => !prevState);
   }
 
   return (
     <main className="h-screen my-8 flex gap-8">
-      <ProjectsSidebar />
-      <NewProject />
+      <ProjectsSidebar onAddNewProject={handleAddNewProject} />
+      {isCreatingNewProject && <NewProject />}
+      {!isCreatingNewProject && <NoProjectSelected />}
     </main>
   );
 }
