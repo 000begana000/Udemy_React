104395ba8dc6da5f67adad5bee386dbea816a20a
000begana000 <begana.contact@gmail.com>
2025-04-07 17:25:49 +0200

    create header & quiz component

---
+++ 000-recap/12-react-quiz/src/components/Quiz.jsx
@@ -0,0 +1,7 @@
+// control this quiz and render other quiz related components
+import { useState } from "react";
+
+export default function Quiz() {
+  // managing state for current question, user answers, and questions
+  return <p>Currently active Question</p>;
+}
