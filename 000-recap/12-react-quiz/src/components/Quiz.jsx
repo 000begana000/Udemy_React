f33147cefaa41edeb2ed04014e24d304dfd03add
000begana000 <begana.contact@gmail.com>
2025-04-08 15:56:39 +0200

    registering answers

--- 000-recap/12-react-quiz/src/components/Quiz.jsx
+++ 000-recap/12-react-quiz/src/components/Quiz.jsx
@@ -1,7 +1,33 @@
 // control this quiz and render other quiz related components
 import { useState } from "react";
 
+import QUESTIONS from "../questions.js";
+
 export default function Quiz() {
   // managing state for current question, user answers, and questions
-  return <p>Currently active Question</p>;
+  const activeQuestionIndex = 0;
+  const [userAnswers, setUserAnswers] = useState([]);
+
+  function handleClickAnswer(selectedAnswer) {
+    setUserAnswers(prevUserAnswers => {
+      return [...prevUserAnswers, selectedAnswer];
+    });
+  }
+
+  return (
+    <div id="quiz">
+      <div id="question">
+        <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
+        <ul id="answers">
+          {QUESTIONS[activeQuestionIndex].answers.map(answer => (
+            <li key={answer} className="answer">
+              <button onClick={() => handleClickAnswer(answer)}>
+                {answer}
+              </button>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
 }
