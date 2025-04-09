3d32525e3c79bd819122107cb4e03cb8c2143131
000begana000 <begana.contact@gmail.com>
2025-04-09 14:46:36 +0200

    highlighting selected answer

--- 000-recap/12-react-quiz/src/components/Quiz.jsx
+++ 000-recap/12-react-quiz/src/components/Quiz.jsx
@@ -6,21 +6,39 @@ import QuestionTimer from "./QuestionTimer.jsx";
 import quizCompeteImg from "../assets/quiz-complete.png";
 
 export default function Quiz() {
-  // managing state for current question, user answers, and questions
+  const [answerState, setAsnwerState] = useState("");
   const [userAnswers, setUserAnswers] = useState([]);
 
-  const activeQuestionIndex = userAnswers.length;
+  // preventing changing length right after when user choosed the answer, instaed let it stay in the same question index
+  const activeQuestionIndex =
+    answerState === "" ? userAnswers.length : userAnswers.length - 1;
   const quizIsComplete = activeQuestionIndex === QUESTIONS.length;
 
   // Memoized to avoid unnecessary re-creation of the function and to optimize performance.
-  const handleSelectAnswer = useCallback(function handleSelectAnswer(
-    selectedAnswer
-  ) {
-    setUserAnswers(prevUserAnswers => {
-      return [...prevUserAnswers, selectedAnswer];
-    });
-  },
-  []);
+  const handleSelectAnswer = useCallback(
+    function handleSelectAnswer(selectedAnswer) {
+      setAsnwerState("answered");
+      setUserAnswers(prevUserAnswers => {
+        return [...prevUserAnswers, selectedAnswer];
+      });
+
+      // define if the user answer is correct or wrong and update state for design purpose
+      setTimeout(() => {
+        if (selectedAnswer === QUESTIONS[activeQuestionIndex].answers[0]) {
+          setAsnwerState("corrent");
+        } else {
+          setAsnwerState("wrong");
+        }
+
+        // reset the answerState to '' after 2 sec
+        setTimeout(() => {
+          setAsnwerState("");
+        }, 2000);
+      }, 1000);
+    },
+    // this function has to be re-created when activeQeustionIndex's value is changed
+    [activeQuestionIndex]
+  );
 
   // Memoized to create a new function that always calls handleSelectAnswer with null as an argument, which may have a specific use case in the application.
   const handleSkipAnswer = useCallback(() => {
@@ -51,13 +69,32 @@ export default function Quiz() {
         />
         <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
         <ul id="answers">
-          {shuffledAnswers.map(answer => (
-            <li key={answer} className="answer">
-              <button onClick={() => handleSelectAnswer(answer)}>
-                {answer}
-              </button>
-            </li>
-          ))}
+          {shuffledAnswers.map(answer => {
+            const isSelected = userAnswers[userAnswers.length - 1] === answer;
+            let cssClasses = "";
+
+            if (answerState === "answered" && isSelected) {
+              cssClasses = "selected";
+            }
+
+            if (
+              (answerState === "correct" || answerState === "wrong") &&
+              isSelected
+            ) {
+              cssClasses = answerState;
+            }
+
+            return (
+              <li key={answer} className="answer">
+                <button
+                  onClick={() => handleSelectAnswer(answer)}
+                  className={cssClasses}
+                >
+                  {answer}
+                </button>
+              </li>
+            );
+          })}
         </ul>
       </div>
     </div>
