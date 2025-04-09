a3bf1fa6282d6f6303e5a9fcf68434625da24ebf
000begana000 <begana.contact@gmail.com>
2025-04-09 14:19:40 +0200

    cleanup functions and key prop

--- 000-recap/12-react-quiz/src/components/QuestionTimer.jsx
+++ 000-recap/12-react-quiz/src/components/QuestionTimer.jsx
@@ -7,7 +7,12 @@ export default function QuestionTimer({ timeout, onTimeout }) {
     console.log("Setting Timout");
 
     // move to the next question when time is out
-    setTimeout(onTimeout, timeout);
+    const timer = setTimeout(onTimeout, timeout);
+
+    // running only 1 timer at the same time
+    return () => {
+      clearTimeout(timer);
+    };
   }, [onTimeout, timeout]);
 
   // to prevent infinite loop - to prevent re-executed all the time but only when the dependencies are changed
@@ -15,9 +20,14 @@ export default function QuestionTimer({ timeout, onTimeout }) {
     console.log("Setting Interval");
 
     // update progress bar
-    setInterval(() => {
+    const interval = setInterval(() => {
       setRemainingTime(prevRemainingTime => prevRemainingTime - 100);
     }, 100);
+
+    // running only 1 interval at the same time
+    return () => {
+      clearInterval(interval);
+    };
   }, []);
 
   return <progress id="question-time" max={timeout} value={remainingTime} />;
