b92f0948dda8545c12cfcca4488c8219b3d0e0e7
000begana000 <begana.contact@gmail.com>
2025-04-11 11:29:22 +0200

    dereiving values outputting questions registering answers

--- 000-recap/12.2-react-quiz-recap/src/components/Quiz.jsx
+++ 000-recap/12.2-react-quiz-recap/src/components/Quiz.jsx
@@ -1,8 +1,30 @@
 import { useState } from "react";
+import QUESTIONS from "../questions";
 
 export default function Quiz() {
-  const [activeQuestionIndex, setActiveQuestionIndex] = useState(0);
   const [userAnswers, setUserAnswers] = useState([]);
 
-  return <p>currently active question</p>;
+  const activeQuestionIndex = userAnswers.length;
+  console.log(activeQuestionIndex);
+
+  function handleSelectAnswer(selectedAnswer) {
+    setUserAnswers(prevUserAnswers => [...prevUserAnswers, selectedAnswer]);
+  }
+
+  return (
+    <div id="quiz">
+      <div id="question">
+        <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
+        <ul id="answers">
+          {QUESTIONS[activeQuestionIndex].answers.map(answer => (
+            <li key={answer} className="answer">
+              <button onClick={() => handleSelectAnswer(selectedAnswer)}>
+                {answer}
+              </button>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
 }
