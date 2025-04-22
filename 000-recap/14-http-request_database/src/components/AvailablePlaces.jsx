d4475c73afcf4a8248f6fc7640cf0ce43cbb31f6
000begana000 <begana.contact@gmail.com>
2025-04-22 17:24:18 +0200

    how to run backend server

--- 000-recap/14-http-request_database/src/components/AvailablePlaces.jsx
+++ 000-recap/14-http-request_database/src/components/AvailablePlaces.jsx
@@ -1,10 +1,18 @@
-import Places from './Places.jsx';
+import { useState } from "react";
+
+const places = localStorage.getItem("places");
+
+import Places from "./Places.jsx";
 
 export default function AvailablePlaces({ onSelectPlace }) {
+  const [availablePlaces, setAvailablePlaces] = useState([]);
+
+  // update UI with HTTP request
+
   return (
     <Places
       title="Available Places"
-      places={[]}
+      places={availablePlaces}
       fallbackText="No places available."
       onSelectPlace={onSelectPlace}
     />
