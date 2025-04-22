76d806e80aa1bf37e9d70ee00d96bfa07f54c820
000begana000 <begana.contact@gmail.com>
2025-04-22 17:43:30 +0200

    how not to end HTTP requests

--- 000-recap/14-http-request_database/src/components/AvailablePlaces.jsx
+++ 000-recap/14-http-request_database/src/components/AvailablePlaces.jsx
@@ -1,5 +1,6 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
+// get the places info from browser local storage
 const places = localStorage.getItem("places");
 
 import Places from "./Places.jsx";
@@ -7,7 +8,15 @@ import Places from "./Places.jsx";
 export default function AvailablePlaces({ onSelectPlace }) {
   const [availablePlaces, setAvailablePlaces] = useState([]);
 
-  // update UI with HTTP request
+  // update UI with get request
+  fetch("http://localhost:3000/places")
+    .then(response => {
+      return response.json(); //returns another promise
+    })
+    .then(resData => {
+      console.log(resData);
+      setAvailablePlaces(resData.places);
+    });
 
   return (
     <Places
