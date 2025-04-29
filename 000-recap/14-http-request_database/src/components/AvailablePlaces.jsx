f258364c1fc9c83b659753c9836ea6e78ac4b494
000begana000 <begana.contact@gmail.com>
2025-04-29 11:45:25 +0200

    Extracting code & improving code structure

--- 000-recap/14-http-request_database/src/components/AvailablePlaces.jsx
+++ 000-recap/14-http-request_database/src/components/AvailablePlaces.jsx
@@ -6,6 +6,7 @@ const places = localStorage.getItem("places");
 import Places from "./Places.jsx";
 import ErrorPage from "./Error.jsx";
 import { sortPlacesByDistance } from "../loc.js";
+import { fetchAvailablePlaces } from "../http.js";
 
 export default function AvailablePlaces({ onSelectPlace }) {
   const [isFetching, setIsFetching] = useState(false);
@@ -17,17 +18,13 @@ export default function AvailablePlaces({ onSelectPlace }) {
       setIsFetching(true);
 
       try {
-        const response = await fetch("http://localhost:3000/places");
-        const resData = await response.json();
-
-        if (!response.ok) {
-          throw new Error("Failed to fetch places");
-        }
+        // this will return promise so await it.
+        const places = await fetchAvailablePlaces();
 
         // works at some point when we got the position
         navigator.geolocation.getCurrentPosition(position => {
           const sortedPlaces = sortPlacesByDistance(
-            resData.places,
+            places,
             position.coords.latitude,
             position.coords.longitude
           );
