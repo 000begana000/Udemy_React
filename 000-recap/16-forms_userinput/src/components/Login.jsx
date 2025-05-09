457864005ad0c68f4eb224dad4aac2b72da87b47
000begana000 <begana.contact@gmail.com>
2025-05-09 13:46:14 +0200

    Getting user input via Refs

--- 000-recap/16-forms_userinput/src/components/Login.jsx
+++ 000-recap/16-forms_userinput/src/components/Login.jsx
@@ -1,22 +1,14 @@
-import { useState } from "react";
+import { useRef } from "react";
 
 export default function Login() {
-  const [enteredValues, setEnteredValues] = useState({
-    email: "",
-    password: "",
-  });
+  const email = useRef();
+  const password = useRef();
 
   function handleSubmit(event) {
     event.preventDefault();
-    console.log(enteredValues);
-  }
-
-  // onchange occurs when the element loses focus, after the content has been changed
-  function handleInputChange(identifier, value) {
-    setEnteredValues(prevState => ({
-      ...prevState,
-      [identifier]: value, //already extracted from onChange event
-    }));
+    const enteredEmail = email.current.value;
+    const enteredPassword = password.current.value;
+    console.log(enteredEmail, enteredPassword);
   }
 
   return (
@@ -26,26 +18,12 @@ export default function Login() {
       <div className="control-row">
         <div className="control no-margin">
           <label htmlFor="email">Email</label>
-          <input
-            id="email"
-            type="email"
-            name="email"
-            onChange={event => handleInputChange("email", event.target.value)}
-            value={enteredValues.email}
-          />
+          <input id="email" type="email" name="email" ref={email} />
         </div>
 
         <div className="control no-margin">
           <label htmlFor="password">Password</label>
-          <input
-            id="password"
-            type="password"
-            name="password"
-            onChange={event =>
-              handleInputChange("password", event.target.value)
-            }
-            value={enteredValues.password}
-          />
+          <input id="password" type="password" name="password" ref={password} />
         </div>
       </div>
 
