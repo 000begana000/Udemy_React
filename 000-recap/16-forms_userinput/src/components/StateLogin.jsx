ca80913bca79062e9a299eea695bee2d520fced0
000begana000 <begana.contact@gmail.com>
2025-05-13 12:40:11 +0200

    Creating a cutom useinput hook

--- 000-recap/16-forms_userinput/src/components/StateLogin.jsx
+++ 000-recap/16-forms_userinput/src/components/StateLogin.jsx
@@ -1,61 +1,31 @@
-import { useState } from "react";
-
 import Input from "./Input";
 import { isEmail, isNotEmpty, hasMinLength } from "../util/validation";
+import { useInput } from "../hooks/useInput";
 
 export default function StateLogin() {
-  const [enteredValues, setEnteredValues] = useState({
-    email: "",
-    password: "",
-  });
+  // email
+  const {
+    value: emailValue,
+    handleInputChange: handleEmailChange,
+    handleInputBlur: handleEmailBlur,
+    hasError: emailHasError,
+  } = useInput("", value => isEmail(value) && isNotEmpty(value)); // multiple validation functions
 
-  const [didEdit, setDidEdit] = useState({
-    email: false,
-    password: false,
-  });
-
-  // validate after the user has edited the field
-  const emailIsInvalid =
-    didEdit.email &&
-    !isEmail(enteredValues.email) &&
-    !isNotEmpty(enteredValues.email);
-
-  const passwordIsInvalid =
-    didEdit.password && !hasMinLength(enteredValues.password, 6);
+  const {
+    value: passwordValue,
+    handleInputBlur: handlePasswordBlur,
+    handleInputChange: handlePasswordChange,
+    hasError: passwordHasError,
+  } = useInput("", value => hasMinLength(value, 6));
 
   function handleSubmit(event) {
     event.preventDefault();
 
-    if (emailIsInvalid) {
+    if (emailHasError || passwordHasError) {
       return; // Early return to prevent further execution
     }
+
     console.log(enteredValues);
-
-    setEnteredValues({
-      email: "",
-      password: "",
-    });
-  }
-
-  // onchange is called by every keystroke
-  function handleInputChange(identifier, value) {
-    setEnteredValues(prevState => ({
-      ...prevState,
-      [identifier]: value, //already extracted from onChange event
-    }));
-
-    // reset the validation when the user starts to type again
-    setDidEdit(prevEdit => ({
-      ...prevEdit,
-      [identifier]: false,
-    }));
-  }
-
-  function handleInputBlur(identifier) {
-    setDidEdit(prevEdit => ({
-      ...prevEdit,
-      [identifier]: true,
-    }));
   }
 
   return (
@@ -68,20 +38,20 @@ export default function StateLogin() {
           id="email"
           type="email"
           name="email"
-          onBlur={() => handleInputBlur("email")}
-          onChange={event => handleInputChange("email", event.target.value)}
-          value={enteredValues.email}
-          error={emailIsInvalid && "Please enter a valid email address."}
+          onBlur={handleEmailBlur}
+          onChange={handleEmailChange}
+          value={emailValue}
+          error={emailHasError && "Please enter a valid email."}
         />
         <Input
           label="Password"
           id="password"
           type="password"
           name="password"
-          onBlur={() => handleInputBlur("password")}
-          onChange={event => handleInputChange("password", event.target.value)}
-          value={enteredValues.password}
-          error={passwordIsInvalid && "Password must be at least 6 characters."}
+          onBlur={handlePasswordBlur}
+          onChange={handlePasswordChange}
+          value={passwordValue}
+          error={passwordHasError && "Password must be at least 6 characters."}
         />
       </div>
 
