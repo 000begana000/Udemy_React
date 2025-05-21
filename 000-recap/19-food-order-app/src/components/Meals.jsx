21ba3d6f87c77385ccd3da1e2d665848ae8ec4b9
000begana000 <begana.contact@gmail.com>
2025-05-21 10:56:20 +0200

    fetching meals data

---
+++ 000-recap/19-food-order-app/src/components/Meals.jsx
@@ -0,0 +1,29 @@
+import { useState, useEffect } from "react";
+
+export default function Meals() {
+  const [meals, setMeals] = useState([]);
+
+  useEffect(() => {
+    async function fetchMeals() {
+      const response = await fetch("http://localhost:3000/meals");
+
+      if (!response.ok) {
+        return;
+      }
+
+      const mealsData = await response.json();
+
+      setMeals(mealsData);
+    }
+
+    fetchMeals();
+  }, []);
+
+  return (
+    <ul id="meals">
+      {meals.map(meal => (
+        <li key={meal.id}>{meal.name}</li>
+      ))}
+    </ul>
+  );
+}
