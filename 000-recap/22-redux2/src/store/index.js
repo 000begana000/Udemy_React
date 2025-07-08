3ec8ff8d292b64eceae6b853103bbef37d0a0cdd
000begana000 <begana.contact@gmail.com>
2025-07-08 13:16:02 +0200

    Connecting redux toolkit state

--- 000-recap/22-redux2/src/store/index.js
+++ 000-recap/22-redux2/src/store/index.js
@@ -1,22 +1,17 @@
-import { createStore } from "redux";
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, configureStore } from "@reduxjs/toolkit";
 
 const initialState = { counter: 0, showCounter: true };
 
-// prepare slice of our global state
-createSlice({
+const counterSlice = createSlice({
   name: "counter",
   initialState,
   reducers: {
     increment(state) {
-      // we can mutate the state here
-      state.counter++; // redux toolkit will clone the state automatically so we don't need to worry about mutation
-      // redux toolkit also keeps the states we don't use but apply changes only
+      state.counter++;
     },
     decrement(state) {
       state.counter--;
     },
-    // action is needed when we have payload only
     increase(state, action) {
       state.counter = state.counter + action.amount;
     },
@@ -26,38 +21,9 @@ createSlice({
   },
 });
 
-const counterReducer = (state = initialState, action) => {
-  if (action.type === "increment") {
-    return {
-      counter: state.counter + 1,
-      showCounter: state.showCounter,
-    };
-  }
-
-  if (action.type === "increse") {
-    return {
-      counter: state.counter + action.amount,
-      showCounter: state.showCounter,
-    };
-  }
-
-  if (action.type === "decrement") {
-    return {
-      counter: state.counter - 1,
-      showCounter: state.showCounter,
-    };
-  }
-
-  if (action.type === "toggle") {
-    return {
-      showCounter: !state.showCounter,
-      counter: state.counter,
-    };
-  }
-
-  return state;
-};
-
-const store = createStore(counterReducer);
+// configuration : 배열, 환경설정
+const store = configureStore({
+  reducer: counterSlice.reducer,
+});
 
 export default store;
