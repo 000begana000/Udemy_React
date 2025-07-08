a2a4a1a934b078b8fe4849f5d8159043dba642ba
000begana000 <begana.contact@gmail.com>
2025-07-08 12:30:13 +0200

    Redux toolkit & adding state slices

--- 000-recap/22-redux2/src/store/index.js
+++ 000-recap/22-redux2/src/store/index.js
@@ -1,6 +1,30 @@
 import { createStore } from "redux";
+import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = { counter: 0, showCounter: true };
+
+// prepare slice of our global state
+createSlice({
+  name: "counter",
+  initialState,
+  reducers: {
+    increment(state) {
+      // we can mutate the state here
+      state.counter++; // redux toolkit will clone the state automatically so we don't need to worry about mutation
+      // redux toolkit also keeps the states we don't use but apply changes only
+    },
+    decrement(state) {
+      state.counter--;
+    },
+    // action is needed when we have payload only
+    increase(state, action) {
+      state.counter = state.counter + action.amount;
+    },
+    toggle(state) {
+      state.showCounter = !state.showCounter;
+    },
+  },
+});
 
 const counterReducer = (state = initialState, action) => {
   if (action.type === "increment") {
