e4b711729ce927481ced4b0dbcdd0e4b9dce089a
000begana000 <begana.contact@gmail.com>
2025-07-20 10:24:01 +0200

    recap1

--- 000-recap/23-advanced-redux/src/components/Cart/CartButton.js
+++ 000-recap/23-advanced-redux/src/components/Cart/CartButton.js
@@ -1,8 +1,17 @@
-import classes from './CartButton.module.css';
+import { useDispatch } from "react-redux";
 
-const CartButton = (props) => {
+import { uiActions } from "../../store/ui-slice";
+import classes from "./CartButton.module.css";
+
+const CartButton = props => {
+  const dispatch = useDispatch();
+
+  const toggleCartHanlder = () => {
+    dispatch(uiActions.toggle());
+  };
+
   return (
-    <button className={classes.button}>
+    <button className={classes.button} onClick={toggleCartHanlder}>
       <span>My Cart</span>
       <span className={classes.badge}>1</span>
     </button>
