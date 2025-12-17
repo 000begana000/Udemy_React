175509d81434ec2b6eb0260b7afe67882acc62a9
000begana000 <begana.contact@gmail.com>
2025-12-17 13:29:07 +0100

    find error and reset back everything to practice

--- 01-food_order_app/src/components/Header.jsx
+++ 01-food_order_app/src/components/Header.jsx
@@ -1,16 +1,7 @@
-import { useContext } from "react";
-
 import logo from "../assets/logo.jpg";
 import Button from "./UI/Button";
-import CartContext from "../store/CartContext";
 
 export default function Header() {
-  const cartCtx = useContext(CartContext);
-
-  const totalCartItems = cartCtx.items.reduce((totalNumberOfItems, item) => {
-    return totalNumberOfItems;
-  }, 0);
-
   return (
     <header id="main-header">
       <div id="title">
@@ -18,7 +9,7 @@ export default function Header() {
         <h1>ReactFood</h1>
       </div>
       <nav>
-        <Button textOnly>Cart ({totalCartItems})</Button>
+        <Button textOnly>Cart (0)</Button>
       </nav>
     </header>
   );
