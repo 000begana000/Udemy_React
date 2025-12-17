52715cb40d4665a84e6610991cd3b1a8769dc2d7
000begana000 <begana.contact@gmail.com>
2025-12-17 16:17:57 +0100

    recap

--- 01-food_order_app/src/store/CartContext.jsx
+++ 01-food_order_app/src/store/CartContext.jsx
@@ -1,1 +1,62 @@
+import { createContext, useReducer } from "react";
 
+const CartContext = createContext({
+  items: [],
+  addItem: item => {},
+  removeItem: id => {},
+});
+
+function cartReducer(state, action) {
+  if (action.type === "ADD_ITEM") {
+    const existingCartItemIndex = state.items.findIndex(
+      item => item.id === action.item.id
+    );
+
+    const updatedItems = [...state.items];
+
+    if (existingCartItemIndex > -1) {
+      const existingCartItem = state.items[existingCartItemIndex];
+
+      const updatedItem = {
+        ...existingCartItem,
+        quantity: existingCartItem.quantity + 1,
+      };
+
+      updatedItems[existingCartItemIndex] = updatedItem;
+    } else {
+      updatedItems.push({ ...action.item, quantity: 1 });
+    }
+
+    return { ...state, items: updatedItems };
+  }
+
+  if (action.type === "REMOVE_ITEM") {
+    //...
+  }
+
+  return state;
+}
+
+export function CartContextProvider({ children }) {
+  const [cart, dispatchCartAction] = useReducer(cartReducer, { items: [] });
+
+  function addItem(item) {
+    dispatchCartAction({
+      type: "ADD_ITEM",
+      item,
+    });
+  }
+
+  const ctxValue = {
+    items: cart.items,
+    addItem,
+  };
+
+  console.log(CartContext);
+
+  return (
+    <CartContext.Provider value={ctxValue}>{children}</CartContext.Provider>
+  );
+}
+
+export default CartContext;
