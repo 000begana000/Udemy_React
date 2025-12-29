0713c7d6cc5ece027296671b21c00210d8cef53d
000begana000 <begana.contact@gmail.com>
2025-12-29 16:22:49 +0100

    feat: extracting fetchAvailablePlaces function

--- practice-http_modal_customhook/src/App.jsx
+++ practice-http_modal_customhook/src/App.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 
 import ErrorPage from "./components/ErrorPage";
+import { fetchAvailablePlaces } from "./http";
 
 function App() {
   const [isFetching, setIsFetching] = useState(false);
@@ -12,14 +13,8 @@ function App() {
       setIsFetching(true);
 
       try {
-        const response = await fetch("http://localhost:3000/places");
-        const resData = await response.json();
-
-        if (!response.ok) {
-          throw new Error("failed to fetch available places.");
-        }
-
-        setAvailablePlaces(resData.places);
+        const places = await fetchAvailablePlaces();
+        setAvailablePlaces(places);
       } catch (error) {
         setError({ message: error.message || "failed to fetch data" });
       }
