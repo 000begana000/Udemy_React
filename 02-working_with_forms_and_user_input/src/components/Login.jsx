b71221284fc9962e65348489176510ae6531f85b
000begana000 <begana.contact@gmail.com>
2026-01-03 11:39:04 +0100

    256

--- 02-working_with_forms_and_user_input/src/components/Login.jsx
+++ 02-working_with_forms_and_user_input/src/components/Login.jsx
@@ -1,7 +1,25 @@
+import { useState } from "react";
+
 export default function Login() {
+  // const [enteredEmail, setEnteredEmail] = useState("");
+  // const [enteredPassword, setEneteredPassword] = useState('')
+
+  const [enteredValues, setEnteredValues] = useState({
+    email: "",
+    password: "",
+  });
+
   function handleSubmit(event) {
     event.preventDefault();
-    console.log("submitted!");
+
+    console.log(enteredValues);
+  }
+
+  function handleInputChange(identifier, event) {
+    setEnteredValues(prevValues => ({
+      ...prevValues,
+      [identifier]: event.target.value,
+    }));
   }
 
   return (
@@ -11,12 +29,24 @@ export default function Login() {
       <div className="control-row">
         <div className="control no-margin">
           <label htmlFor="email">Email</label>
-          <input id="email" type="email" name="email" />
+          <input
+            id="email"
+            type="email"
+            name="email"
+            onChange={event => handleInputChange("email", event)}
+            value={enteredValues.email}
+          />
         </div>
 
         <div className="control no-margin">
           <label htmlFor="password">Password</label>
-          <input id="password" type="password" name="password" />
+          <input
+            id="password"
+            type="password"
+            name="password"
+            onChange={event => handleInputChange("password", event)}
+            value={enteredValues.password}
+          />
         </div>
       </div>
 
